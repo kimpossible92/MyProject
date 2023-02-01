71fa51949e2715f832e642839de3cec048ed30d1
kimpossible92 <kim.possible.92@bk.ru>
2023-02-01 19:44:07 +0900

    dfsdf

--- Source/MyProject/EnemyComp.cpp
+++ Source/MyProject/EnemyComp.cpp
@@ -9,7 +9,7 @@ UEnemyComp::UEnemyComp()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	the100 = 30.0f;
+	the100 = 10.0f;
 	// ...
 }
 
